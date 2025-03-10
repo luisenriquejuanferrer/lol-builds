51730ae66b13235505199fea22752fa302620dc2
LUIS <luisenriquejuanf@gmail.com>
2025-03-10 20:18:55 +0100

    Initialize Spring Boot application with Maven setup and necessary configurations

---
+++ src/main/java/com/leaguebuilds/LolBuildsApplication.java
@@ -0,0 +1,13 @@
+package com.leaguebuilds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolBuildsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LolBuildsApplication.class, args);
+    }
+
+}
