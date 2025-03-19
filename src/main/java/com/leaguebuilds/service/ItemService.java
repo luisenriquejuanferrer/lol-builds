3f0e4ae70c81d402118f5890643202c1ceea65ff
LUIS <luisenriquejuanf@gmail.com>
2025-03-19 22:35:27 +0100

    Add method to remove duplicate items in ItemService and update comments

--- src/main/java/com/leaguebuilds/service/ItemService.java
+++ src/main/java/com/leaguebuilds/service/ItemService.java
@@ -11,6 +11,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class ItemService {
@@ -25,7 +29,7 @@ public class ItemService {
 
     /**
      * Carga los ítems desde la API de Riot Games y los almacena en una lista.
-     * Este método se ejecuta automáticamente después de que el contenedor de Spring haya inicializado el bean.
+     * Esta función se ejecuta automáticamente después de que el contenedor de Spring haya inicializado el bean.
      */
     @PostConstruct
     public void loadItems() {
@@ -34,8 +38,10 @@ public class ItemService {
             JsonNode root = objectMapper.readTree(response);
             JsonNode dataNode = root.path("data");
             dataNode.fields().forEachRemaining(entry -> {
+
                 String id = entry.getKey();
                 JsonNode itemNode = entry.getValue();
+
                 if (itemNode.path("maps").path("11").asBoolean() && itemNode.path("gold").path("purchasable").asBoolean()) {
                     ((ObjectNode) itemNode).put("id", id);
                     int totalGold = itemNode.path("gold").path("total").asInt();
@@ -43,15 +49,35 @@ public class ItemService {
                     try {
                         Item item = objectMapper.treeToValue(itemNode, Item.class);
                         items.put(Integer.valueOf(item.getId()), item);
-                        //item.setImage(Utils.RIOT_API_IMAGE_URL + item.getId() + ".png");
                     } catch (JsonProcessingException e) {
                         throw new RuntimeException(e);
                     }
                 }
+
             });
         } catch (Exception e) {
             throw new RuntimeException("Error parsing item data: " + e.getMessage());
         }
+    }
+
+    /**
+     * Elimina los ítems duplicados en el HashMap `items` basándose en el nombre del ítem.
+     * Esta función se ejecuta automáticamente después de que el contenedor de Spring haya inicializado el bean.
+     *
+     * Utiliza un `Set` para rastrear los nombres de los ítems que ya se han visto y filtra los duplicados.
+     * Luego, limpia el HashMap original y lo vuelve a llenar con los ítems únicos.
+     */
+    @PostConstruct
+    public void deleteDuplicatedItems() {
+        Set<String> seenNames = new HashSet<>();
+
+        Map<Integer, Item> filteredItems = items.entrySet()
+                .stream()
+                .filter(entry -> seenNames.add(entry.getValue().getName()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        items.clear();
+        items.putAll(filteredItems);
     }
 
     public Item getItemById(String id) {
