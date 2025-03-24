dd4798330d78d81dc32b4c3b04a23347bf0eb199
LUIS <luisenriquejuanf@gmail.com>
2025-03-24 23:41:03 +0100

    Update RIOT API version to 15.6.1 in Utils class

--- src/main/java/com/leaguebuilds/utils/Utils.java
+++ src/main/java/com/leaguebuilds/utils/Utils.java
@@ -2,7 +2,7 @@ package com.leaguebuilds.utils;
 
 public class Utils {
     public static final String BASE_API_URL = "/api/lol";
-    private static final String RIOT_API_VERSION = "15.5.1";
+    private static final String RIOT_API_VERSION = "15.6.1";
     public static final String RIOT_API_ITEM_URL = "https://ddragon.leagueoflegends.com/cdn/" + RIOT_API_VERSION + "/data/en_US/item.json";
     public static final String RIOT_API_CHAMPION_URL = "https://ddragon.leagueoflegends.com/cdn/" + RIOT_API_VERSION + "/data/en_US/item.json";
     public static final String RIOT_API_ITEM_IMAGE_URL = "https://ddragon.leagueoflegends.com/cdn/" + RIOT_API_VERSION + "/img/item/";
